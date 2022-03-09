ceea1b6b04dae038f209ec432c88cf62406fbc9d
Davi <davi.mirandagonc@gmail.com>
2022-03-09 20:13:17 -0300

    First commit

---
+++ src/main/kotlin/br/com/dg/panteao/cronus/CronusApplication.kt
@@ -0,0 +1,11 @@
+package br.com.dg.panteao.cronus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CronusApplication
+
+fun main(args: Array<String>) {
+	runApplication<CronusApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/dg/panteao/cronus/CronusApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.dg.panteao.cronus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CronusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
