50de9809b77f4be4c9d252192ca31d139fdfc59b
Davi <davi.mirandagonc@gmail.com>
2022-04-08 23:09:46 -0300

    fix: Corrigindo os application para utilizarem o mesmo tipo de configuração do banco mongo

--- src/main/kotlin/br/com/dg/panteao/cronus/CronusApplication.kt
+++ src/main/kotlin/br/com/dg/panteao/cronus/CronusApplication.kt
@@ -3,10 +3,10 @@ package br.com.dg.panteao.cronus
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-
 @SpringBootApplication
 class CronusApplication
 
 fun main(args: Array<String>) {
     runApplication<CronusApplication>(*args)
 }
+
